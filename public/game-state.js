ad704a886b412aed1599e4a6c5fadd7215f9acb5
Abimael Garcia <abimael1992g@gmail.com>
2025-08-24 22:30:42 -0600

    remove old game-state.js

--- public/game-state.js
+++
@@ -1,7 +0,0 @@
-export let currentGameState = null;
-export let playerId = null;
-export let currentGameCode = null;
-
-export function updateGameState(newState) {
-    currentGameState = newState;
-}
