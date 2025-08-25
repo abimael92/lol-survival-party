85d9f360d59d2a491b4503727d5d1d66fb128280
Abimael Garcia <abimael1992g@gmail.com>
2025-08-24 22:30:42 -0600

    update server.js with latest game logic

--- server.js
+++ server.js
@@ -566,5 +566,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
 });
