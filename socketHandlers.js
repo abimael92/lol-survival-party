f5eac9e3bcd602bb303c7c159d9020ec15eb7294
Abimael Garcia <abimael1992g@gmail.com>
2025-10-10 15:49:00 -0600

    socketHandlers.js: backend socket logic

---
+++ socketHandlers.js
@@ -0,0 +1,19 @@
+// socketHandlers.js
+const { initGameManager } = require('./gameManager');
+
+function initSocketHandlers(io) {
+    const gameManager = initGameManager(io);
+
+    io.on('connection', socket => {
+        console.log('New client connected:', socket.id);
+
+        socket.on('create-game', playerName => gameManager.handleCreateGame(socket, playerName));
+        socket.on('join-game', data => gameManager.handleJoinGame(socket, data));
+        socket.on('start-game', () => gameManager.handleStartGame(socket));
+        socket.on('submit-action', data => gameManager.handleSubmitAction(socket, data));
+        socket.on('submit-vote', votedPlayerId => gameManager.handleSubmitVote(socket, votedPlayerId));
+        socket.on('disconnect', () => gameManager.handleDisconnect(socket));
+    });
+}
+
+module.exports = { initSocketHandlers };
