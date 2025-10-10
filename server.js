93922ab4a284dd6e084228be24c30cbc9241a498
Abimael Garcia <abimael1992g@gmail.com>
2025-10-10 15:48:57 -0600

    server.js: update server logic

--- server.js
+++ server.js
@@ -1,51 +1,17 @@
 // server.js
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
-const path = require('path');
-const { initGameManager } = require('./gameManager');
+const { Server } = require('socket.io');
+const { initSocketHandlers } = require('./socketHandlers');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = new Server(server);
 
-// Serve static files
 app.use(express.static('public'));
 
-// Initialize game manager
-const gameManager = initGameManager(io);
+// Initialize socket handlers
+initSocketHandlers(io);
 
-// Socket connection handling
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
-
-    socket.on('create-game', (playerName) => {
-        gameManager.handleCreateGame(socket, playerName);
-    });
-
-    socket.on('join-game', (data) => {
-        gameManager.handleJoinGame(socket, data);
-    });
-
-    socket.on('start-game', () => {
-        gameManager.handleStartGame(socket);
-    });
-
-    socket.on('submit-action', (data) => {
-        gameManager.handleSubmitAction(socket, data);
-    });
-
-    socket.on('submit-vote', (votedPlayerId) => {
-        gameManager.handleSubmitVote(socket, votedPlayerId);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-        gameManager.handleDisconnect(socket);
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-});
+const PORT = 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
